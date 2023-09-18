589f69833b606529f37109a6a264b2075dd89aae
Luiz Humberto <luizhumberto@gmail.com>
2023-09-18 14:45:53 -0300

    feat: create dashboard component

---
+++ src/modules/main/components/DashboardComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface Props {
+  userName: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <h1>{{ userName }}</h1>
+  <div>Dashboard</div>
+</template>
--- src/modules/main/views/HomeView.vue
+++ src/modules/main/views/HomeView.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import { useHelpers } from '@/shared/composables'
+import { DashboardComponent } from '../components'
+const { fetchData } = useHelpers()
+const userName = fetchData('userName')
+</script>
 <template>
   <v-container class="d-flex justify-center">
     <v-responsive class="text-center">
-      <h1 class="text-center text-h3">Home Page</h1>
+      <DashboardComponent :user-name="userName" />
     </v-responsive>
   </v-container>
 </template>
