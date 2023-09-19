3fd8ad801d5dc2f6b48cb3b8cafb09392bb42307
Luiz Humberto <luizhumberto@gmail.com>
2023-09-19 15:56:28 -0300

    refactor: change items order

--- src/modules/main/composables/useUser.ts
+++ src/modules/main/composables/useUser.ts
@@ -1,19 +1,25 @@
 import { ref } from 'vue'
+import { useSharedSore } from '@/shared/store'
 import { useHelpers } from '@/shared/composables'
 const { fetchData, deleteItem, delay } = useHelpers()
 const isPending = ref(false)
 
 const userName = ref<string | null>(null)
 const useUser = () => {
+  const { enableSnackbar, disableSnackbar } = useSharedSore()
   const fetchUser = () => {
     userName.value = fetchData('userName')
   }
 
   const logout = async () => {
     isPending.value = true
-    await delay()
     deleteItem('userName')
+    await delay()
     isPending.value = false
+    userName.value = null
+    enableSnackbar('Logout feito com sucesso')
+    await delay(2000)
+    disableSnackbar()
   }
 
   return { userName, fetchUser, logout, isPending }
