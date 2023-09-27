c26c77c67e994c8d5a586a9859049a748a5e119f
Luiz Humberto <luizhumberto@gmail.com>
2023-09-27 12:46:09 -0300

    style: add table style

--- src/modules/main/components/ExpensesTable.vue
+++ src/modules/main/components/ExpensesTable.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Budget, Expense } from '../types'
+import { useHelpers } from '@/shared/composables'
 import { toRefs } from 'vue'
 import { useMain } from '../composables'
 const props = defineProps<Props>()
@@ -9,6 +10,7 @@ interface Props {
 const { expenses } = toRefs(props)
 
 const { getBudget } = useMain()
+const { localCurrency, timestampToDate } = useHelpers()
 
 const goToBudget = (id: Budget['id']) => {
   console.log(id)
@@ -19,8 +21,13 @@ const deleteExpense = (id: Expense['id']) => {
 </script>
 
 <template>
+  <div class="text-h5 text-left font-weight-bold">
+    <span>Despesas recentes</span>
+    <span class="text-subtitle-2">{{ ` (${expenses.length} total)` }}</span>
+  </div>
   <v-data-table
     class="elevation-1"
+    density="compact"
     :headers="[
       {
         title: 'Nome',
@@ -32,31 +39,34 @@ const deleteExpense = (id: Expense['id']) => {
       },
       {
         title: 'Data',
-        key: 'formatDate',
+        key: 'createdAt',
       },
       {
         title: 'Orçamento',
         key: 'budget_id',
+        width: 40,
       },
       {
-        title: '*',
+        title: '',
         key: 'id',
       },
     ]"
-    item-value="name"
     :items="expenses"
+    sort-asc-icon="mdi-sort-ascending"
+    :sort-by="[{ key: 'createdAt', order: 'desc' }]"
+    sort-desc-icon="mdi-sort-descending"
   >
     <template v-slot:item="{ item }">
-      <tr>
+      <tr class="text-left">
         <td>{{ item.columns.name }}</td>
-        <td>{{ item.columns.amount }}</td>
-        <td>{{ item.columns.formatDate }}</td>
-        <td>
-          <v-btn
+        <td>{{ localCurrency(item.columns.amount) }}</td>
+        <td>{{ timestampToDate(item.columns.createdAt) }}</td>
+        <td class="text-center">
+          <v-chip
             :color="getBudget(item.columns.budget_id)?.color"
-            width="150"
+            :density="'compact'"
             @click="goToBudget(item.columns.budget_id)"
-            >{{ getBudget(item.columns.budget_id)?.name }}</v-btn
+            >{{ getBudget(item.columns.budget_id)?.name }}</v-chip
           >
         </td>
         <td>
@@ -68,5 +78,6 @@ const deleteExpense = (id: Expense['id']) => {
         </td>
       </tr>
     </template>
+    <template v-slot:bottom> </template>
   </v-data-table>
 </template>
