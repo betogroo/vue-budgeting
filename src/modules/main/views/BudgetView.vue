c71f205ab27667cf0770546e5e04b981acbfd5f4
betogroo <luizhumberto@gmail.com>
2023-09-29 09:15:13 -0300

    feat: add header addition on expense table in budget view

--- src/modules/main/views/BudgetView.vue
+++ src/modules/main/views/BudgetView.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useMain } from '../composables'
 import { useMainStore } from '../store/useMainStore'
-import { ExpensesTable } from '../components'
-defineProps<Props>()
-const store = useMainStore()
+import { ExpensesTable, BudgetCard, ExpenseForm } from '../components'
+const props = defineProps<Props>()
 interface Props {
   id: string
 }
+const store = useMainStore()
+const { expensesByBudgetId, budgets } = store
+const { getBudget } = useMain()
+const budget = ref(getBudget(props.id))
 </script>
 
 <template>
   <v-container>
     <v-responsive>
-      <ExpensesTable :expenses="store.expensesByBudgetId(id)" />
+      <ExpensesTable
+        :expenses="expensesByBudgetId(id)"
+        :header-addiction="budget?.name"
+      />
+      <BudgetCard
+        v-if="budget"
+        :budget="budget"
+      />
+      <ExpenseForm :budgets="budgets" />
     </v-responsive>
   </v-container>
 </template>
